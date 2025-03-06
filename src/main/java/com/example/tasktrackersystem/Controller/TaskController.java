3d4c3e1ea975e9d354e18a803a67d6ccd7a95de1
AbdullahAli1999 <iifamxx@gmail.com>
2025-03-06 14:32:56 +0300

    Initial commit

---
+++ src/main/java/com/example/tasktrackersystem/Controller/TaskController.java
@@ -0,0 +1,59 @@
+package com.example.tasktrackersystem.Controller;
+
+import com.example.tasktrackersystem.Api.ApiResponse;
+import com.example.tasktrackersystem.Model.Task;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/task")
+public class TaskController {
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    //GET
+    @GetMapping("/get")
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    //ADD
+    @PostMapping("/add")
+    public ApiResponse addTask(@RequestBody Task task) {
+        tasks.add(task);
+        return new ApiResponse("Task added successfully");
+    }
+    //UPDATE
+    @PutMapping("update/{index}")
+    public ApiResponse updateTask(@PathVariable int index, @RequestBody Task task) {
+        tasks.set(index, task);
+        return new ApiResponse("Task updated successfully");
+    }
+
+    //DELETE
+    @DeleteMapping("delete/{index}")
+    public ApiResponse deleteTask(@PathVariable int index) {
+        tasks.remove(index);
+        return new ApiResponse("Task deleted successfully");
+    }
+
+    //CHANGE
+    @PutMapping("change/{index}")
+    public ApiResponse changeStatus(@PathVariable int index, String status) {
+        tasks.get(1).setStatus("not done");
+        return new ApiResponse("Task status changed successfully");
+    }
+
+
+    //SEARCH
+    @GetMapping("/search/{title}")
+    public ApiResponse searchTasks(@PathVariable String title) {
+        for (Task task : tasks) {
+            if (task.getTitle().equalsIgnoreCase(title)) {
+                return new ApiResponse("Task found: " + task.getTitle());
+            }
+        }
+        return new ApiResponse("Task not found");
+    }
+
+}
